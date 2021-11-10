29250f7f4d896297007d6e81a4ef6f75a1e027ac
Klas af Geijerstam <klas.afgeijerstam@slagkryssaren.com>
2021-11-10 22:22:31 +0100

    Added frontend receiver endpoint for token

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,38 @@
-import {useState} from 'react';
-import {Route, Routes} from 'react-router-dom';
+import {Typography} from '@mui/material';
+import {useEffect, useState} from 'react';
+import {Navigate, Route, Routes, useSearchParams } from 'react-router-dom';
 import MainPage from './MainPage';
 
 
 function App() {
   const [token, setToken] = useState<string>();
+
+  const TokenCallbackPage = () => {
+    const [params] = useSearchParams();
+    
+    useEffect(() => {
+      const token = params.get("token");
+      if (token !== null) {
+        setToken(token);
+      }
+    }, [params]);
+    
+
+    return (
+      <>
+        {token !== null ? (
+            <Navigate to="/" />
+          ) : (
+            <Typography variant="h1">No token provided :(</Typography>
+          )
+        }
+      </>
+    );
+  };
+
   return (
     <Routes>
+      <Route path="/callback" element={<TokenCallbackPage />} />
       <Route path="/" element={<MainPage token={token} />} />
     </Routes>
   );
